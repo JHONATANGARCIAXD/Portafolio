a8b712019a356f0152aa81af65638da2b6feec55
Jhonatan Steven García Gómez <102763345+JHONATANGARCIAXD@users.noreply.github.com>
2025-06-17 20:35:01 -0500

    Modificaciones

--- AdministradorCitas/script.js
+++ AdministradorCitas/script.js
@@ -1,4 +1,15 @@
-
+const imagenes = [
+    { name: "perro", url: "./img/perro.png" },
+    { name: "gato", url: "./img/gato.png" },
+    { name: "elefante", url: "./img/elefante.png" },
+    { name: "tigre", url: "./img/tigre.png" },
+    { name: "leon", url: "./img/leon.png" },
+    { name: "jirafa", url: "./img/jirafa.png" },
+    { name: "oso", url: "./img/oso.png" },
+    { name: "canguro", url: "./img/canguro.png" },
+    { name: "lobo", url: "./img/lobo.png" },
+    { name: "aguila", url: "./img/aguila.png" }
+];
 // FORMULARIO
 const NombreMascota = document.getElementById("nombremascota")
 const Propietario = document.getElementById("propietario")
@@ -12,19 +23,27 @@ const Filtro = document.getElementById("filtro")
 const Search = document.getElementById("search")
 const Modal_Formulario = new bootstrap.Modal(document.getElementById("staticBackdrop"))
 const Modal_Eliminar = document.getElementById("staticeliminar")
-const Confirmar_Eliminar = document.getElementById("eliminar")
 const ContCitas = document.getElementById("cont_citas")
 const BtnGuardar = document.getElementById("guardar")
+
 
 // VARIALES GLOBALES
 let validacion = false
 let CitasTotales = JSON.parse(localStorage.getItem("Citas")) || [];
 console.log(CitasTotales);
 let op = 0
-let pos = 0
+let pos
 
 // VALIDACIONES 
 const Validaciones = () => {
+    let fecha_hoy = new Date()
+    let solo_fecha = fecha_hoy.toISOString().split("T")[0]
+    let hora_hot = fecha_hoy.toLocaleTimeString()
+    console.log(hora_hot.split(":")[0]);
+
+
+    let h = Hora.value.split(":")[0]
+    let m = Hora.value.split(":")[1]
     if (NombreMascota.value == "") {
         Swal.fire({
             title: "CAMPO VACIO",
@@ -46,11 +65,32 @@ const Validaciones = () => {
             text: "POR FAVOR COMPLETA EL CAMPO TELEFONO"
         })
     }
+    else if (/[a-zA-Z]/.test(Telefono.value)) {
+        Swal.fire({
+            icon: "warning",
+            title: "VALOR DEL CAMPO INCORRECTO",
+            text: "EL CAMBO TELEFONO SOLO DEBE TENER NUMEROS"
+        })
+    }
+    else if (Telefono.value.length < 10) {
+        Swal.fire({
+            icon: "warning",
+            title: "VALOR DEL CAMPO INCORRECTO",
+            text: "EL CAMBO TELEFONO DEBE TENER MINIMO 10 DIGITOS"
+        })
+    }
     else if (Fecha.value == "") {
         Swal.fire({
             icon: "warning",
             title: "CAMPO VACIO",
             text: "POR FAVOR COMPLETA EL CAMPO FECHA"
+        })
+    }
+    else if (Fecha.value < solo_fecha) {
+        Swal.fire({
+            icon: "warning",
+            title: "ERROR DE VALORES",
+            text: `POR FAVOR PONGA UNA FECHA SUPERIOR`
         })
     }
     else if (Hora.value == "") {
@@ -59,6 +99,13 @@ const Validaciones = () => {
             title: "CAMPO VACIO",
             text: "POR FAVOR COMPLETA EL CAMPO HORA"
         })
+    }
+    else if (h < 8 || h > 20 || (h == 20 && m > 0)) {
+        Swal.fire({
+            icon: "warning",
+            title: "HORARIO INVÁLIDO",
+            text: "POR FAVOR CAMBIA LA HORA, LA VETERINARIA ATIENDE DE 8:00 AM A 8:00 PM"
+        });
     }
     else if (TipoMascota.value == "Seleccione Uno") {
         Swal.fire({
@@ -73,17 +120,18 @@ const Validaciones = () => {
             title: "CAMPO VACIO",
             text: "POR FAVOR COMPLETA EL SINTOMAS"
         })
-    } else {
+    }
+    else {
         validacion = true
+        console.log(Hora.value);
+
     }
 }
-
 // GUARDAR LA CITA EN EL LOCALSTORAGE
 const Guardar = () => {
-    console.log(pos, op);
     Validaciones()
-    if (op == 0) {
-        if (validacion) {
+    if (validacion) {
+        if (op == 0) {
             let numero_cita = Math.floor((new Date() * Math.random()))
             let cita = {
                 NombreMascota: NombreMascota.value,
@@ -98,42 +146,47 @@ const Guardar = () => {
             }
             CitasTotales.unshift(cita)
             localStorage.setItem("Citas", JSON.stringify(CitasTotales))
-            Filtrar()
-            limpiar()
-
             Swal.fire({
                 icon: "success",
                 title: "CITA CREADA CON EXITO"
             })
 
+            validacion = false
             document.querySelector('#staticBackdrop [data-bs-dismiss="modal"]').click()
+            Filtrar()
+            limpiar()
+
+        }
+        else if (op == 1) {
+            console.log(pos);
+            CitasTotales[pos].NombreMascota = NombreMascota.value
+            CitasTotales[pos].Propietario = Propietario.value
+            CitasTotales[pos].Telefono = Telefono.value
+            CitasTotales[pos].Fecha = Fecha.value
+            CitasTotales[pos].Hora = Hora.value
+            CitasTotales[pos].TipoMascota = TipoMascota.value
+            CitasTotales[pos].Sintomas = Sintomas.value
+
+            console.log(`ELEMENTO A MODIFICAR: ${CitasTotales[pos]}, POSICION: ${pos}`);
+
+            BtnGuardar.textContent = "Guardar"
+            localStorage.setItem("Citas", JSON.stringify(CitasTotales));
+            Filtrar()
+            limpiar()
+            op = 0
+
+            Swal.fire({
+                icon: "success",
+                title: "CITA EDITADA CON EXITO"
+            })
+
+            document.querySelector('#staticBackdrop [data-bs-dismiss="modal"]').click()
+            validacion = false
 
         }
     }
-    else if (op == 1) {
-        console.log(pos);
-        CitasTotales[pos].NombreMascota = NombreMascota.value
-        CitasTotales[pos].Propietario = Propietario.value
-        CitasTotales[pos].Telefono = Telefono.value
-        CitasTotales[pos].Fecha = Fecha.value
-        CitasTotales[pos].Hora = Hora.value
-        CitasTotales[pos].TipoMascota = TipoMascota.value
-        CitasTotales[pos].Sintomas = Sintomas.value
 
 
-        BtnGuardar.textContent = "Guardar"
-        localStorage.setItem("Citas", JSON.stringify(CitasTotales));
-        Filtrar()
-        limpiar()
-        op = 0
-
-        Swal.fire({
-            icon: "success",
-            title: "CITA EDITADA CON EXITO"
-        })
-
-        document.querySelector('#staticBackdrop [data-bs-dismiss="modal"]').click()
-    }
 
 }
 
@@ -175,25 +228,37 @@ const PintarCita = (CitasAMostrar) => {
         return;
     } else {
         ContCitas.innerHTML = "";
-        CitasAMostrar.sort((a, b) => a < b ? -1 : 1)
+        CitasAMostrar.sort((a, b) => a.Hora < b.Hora ? -1 : 1)
+        CitasAMostrar.sort((a, b) => a.Fecha < b.Fecha ? -1 : 1)
         CitasAMostrar.forEach((elemento) => {
+            let url2 = imagenes.find((imagen) => { return imagen.name == elemento.TipoMascota })
             ContCitas.innerHTML += `
                 <div class="CardCita">
-                    <h3>${elemento.Numero}</h3>
-                    <h1>${elemento.NombreMascota}</h1>
-                    <p>Propietario: ${elemento.Propietario}</p>
-                    <p>Telefono: ${elemento.Telefono}</p>
-                    <p>Fecha: ${elemento.Fecha}</p>
-                    <p>Hora: ${elemento.Hora}</p>
-                    <p>Sintomas: ${elemento.Sintomas}</p>
-                    <select class="form-select" name="select_estado">
-                        <option value="Abierta">Abierta</option>
-                        <option value="Terminada">Terminada</option>
-                        <option value="Anulada">Anulada</option>
-                    </select>
+                    <div class="contenido_cita">
+                        <h3 class="numero_cita">#${elemento.Numero}</h3>
+                        <div class="cont_img_nombre">
+                        <h2>${elemento.NombreMascota}</h2>
+                        <img src = ${url2.url} class="img_animal">
+                        </div>
+                        <div class="info_conte">
+                            <p> <i class="bi bi-person-fill"></i> <span class="negrilla">Propietario: </span> ${elemento.Propietario.toUpperCase()}</p>
+                            <p> <i class="bi bi-telephone-fill"></i> <span class="negrilla">Telefono: </span> ${elemento.Telefono}</p>
+                            <p> <i class="bi bi-calendar2-week-fill"></i> <span class="negrilla">Fecha: </span> ${elemento.Fecha}</p>
+                            <p> <i class="bi bi-clock-fill"></i> <span class="negrilla">Hora: </span>${elemento.Hora}</p>
+                            <p> <i class="bi bi-clipboard2-heart-fill"></i>  <span class="negrilla">Sintomas: </span> ${elemento.Sintomas}</p>
+                        </div>
+                    </div>
                     <div>
-                        <button name="editar">📝</button>
-                        <button name="eliminar" data-bs-toggle="modal" data-bs-target="#staticBackdrop2">❌</button>
+                        <label>Estado:</label>
+                        <select class="form-select" name="select_estado">
+                            <option value="Abierta">Abierta</option>
+                            <option value="Terminada">Terminada</option>
+                            <option value="Anulada">Anulada</option>
+                        </select>
+                    </div>
+                    <div class="cont_buttons">
+                        <button name="editar">Editar</button>
+                        <button name="eliminar" data-bs-toggle="modal" data-bs-target="#staticBackdrop2">Eliminar</button>
                     </div>
                 </div>
             `;
@@ -204,29 +269,31 @@ const PintarCita = (CitasAMostrar) => {
         const botonesEditar = ContCitas.querySelectorAll("button[name='editar']");
 
         selects.forEach((select, i) => {
+            const numeroCita = CitasAMostrar[i].Numero;
+            const index = CitasTotales.findIndex(cita => cita.Numero === numeroCita);
             select.value = CitasAMostrar[i].Estado;
-
             select.addEventListener("change", () => {
-                CitasTotales[i].Estado = select.value;
+                console.log(index);
+                CitasTotales[index].Estado = select.value;
                 localStorage.setItem("Citas", JSON.stringify(CitasTotales));
                 Filtrar();
-
             });
         });
 
         botonesEditar.forEach((btn, i) => {
+            const numeroCita = CitasAMostrar[i].Numero;
+            const index = CitasTotales.findIndex(cita => cita.Numero === numeroCita);
             btn.addEventListener("click", () => {
                 BtnGuardar.textContent = "Editar";
                 op = 1;
-                pos = i
-                const cita = CitasTotales[pos];
-                NombreMascota.value = cita.NombreMascota;
-                Propietario.value = cita.Propietario;
-                Telefono.value = cita.Telefono;
-                Fecha.value = cita.Fecha;
-                Hora.value = cita.Hora;
-                TipoMascota.value = cita.TipoMascota;
-                Sintomas.value = cita.Sintomas;
+                pos = index
+                NombreMascota.value = CitasTotales[index].NombreMascota;
+                Propietario.value = CitasTotales[index].Propietario;
+                Telefono.value = CitasTotales[index].Telefono;
+                Fecha.value = CitasTotales[index].Fecha;
+                Hora.value = CitasTotales[index].Hora;
+                TipoMascota.value = CitasTotales[index].TipoMascota;
+                Sintomas.value = CitasTotales[index].Sintomas;
 
                 Modal_Formulario.toggle();
             });
